35d73d62af266448539c6296a4f38a3838152474
dimmaryanto93 <software.dimas_m93@icloud.com>
2018-12-07 15:08:16 +0700

    init project springboot

---
+++ src/main/java/com/maryanto/dimas/example/SpringbootJasperIntegration.java
@@ -0,0 +1,12 @@
+package com.maryanto.dimas.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJasperIntegration {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJasperIntegration.class, args);
+	}
+}
